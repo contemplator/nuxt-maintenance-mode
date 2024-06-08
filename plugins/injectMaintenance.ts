fc41a8e78e343899ebcb156ba041c91adb95cc80
Leo Lin <contemplation8213@gmail.com>
2024-06-08 15:36:02 +0800

    fix: remove useless client maintenance mode judgment

--- plugins/injectMaintenance.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtPlugin(async () => {
-  const asyncData = await useFetch("/website-api/maintenance");
-  const isMaintenanceMode = asyncData.data.value as boolean;
-  const maintenanceMode = useState("maintenanceMode", () => false);
-  maintenanceMode.value = isMaintenanceMode;
-
-  return;
-});
